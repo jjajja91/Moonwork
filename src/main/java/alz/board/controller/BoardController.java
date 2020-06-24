e0fdebaa2f09f6eb98106b016f131800870b733d
jjajja91 <jjajja91@hanmail.net>
2020-06-24 13:00:22 +0900

    test commit

--- src/main/java/alz/board/controller/BoardController.java
+++ src/main/java/alz/board/controller/BoardController.java
@@ -1,11 +1,24 @@
 package alz.board.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import alz.board.domain.BoardDTO;
 
 public interface BoardController {
 	
-	public void register(BoardDTO board, RedirectAttributes rttr);
+	public void list(Model model);
+	
+	public void listByWriter(Model model);
+	
+	public String register(BoardDTO board, RedirectAttributes rttr);
+	
+	public void register();
+	
+	public void get(long id, Model model);
+	
+	public String modify(BoardDTO board, RedirectAttributes rttr);
+	
+	public String remove(long id, RedirectAttributes rttr);
 
 }
